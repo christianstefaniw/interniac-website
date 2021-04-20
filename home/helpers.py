bd3f1b210ecdfa949553c397586a5b4b514354f7
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-04-20 14:35:54 -0400

    figuring out google auth

--- home/helpers.py
+++ home/helpers.py
@@ -5,11 +5,18 @@ from django.utils import timezone
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+import settings
 
 def insert_into_spreadsheet(email) -> None:
+    if settings.DEBUG:
+        creds_file = ServiceAccountCredentials.from_json_keyfile_name(
+        os.path.join(Path(__file__).resolve().parent.parent, 'client_secret.json'), scope)
+    else:
+        creds_file = ServiceAccountCredentials.from_json_keyfile_name(
+        os.path.join(Path(__file__).resolve().parent.parent, '../google-credentials.json'), scope)
+
     scope = ['https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        os.path.join(Path(__file__).resolve().parent.parent, 'google-credentials.json'), scope)
+    creds = creds_file
     client = gspread.authorize(creds)
     sheet = client.open("Join Interniac (Responses)").sheet1
     if not _email_exists(email, sheet):
