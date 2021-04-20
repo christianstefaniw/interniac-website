38b9f18584b6ff016e7482179ab06cfa619721eb
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-04-20 15:12:55 -0400

    google creds

--- home/helpers.py
+++ home/helpers.py
@@ -7,21 +7,15 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 from connect_x import settings
 
+
 def insert_into_spreadsheet(email) -> None:
     scope = ['https://www.googleapis.com/auth/drive']
-    if settings.DEBUG:
-        creds_file = ServiceAccountCredentials.from_json_keyfile_name(
-        os.path.join(Path(__file__).resolve().parent.parent, 'client_secret.json'), scope)
-    else:
-        creds_file = ServiceAccountCredentials.from_json_keyfile_name(
-        os.path.join(Path(__file__).resolve().parent.parent, 'google-credentials.json'), scope)
-    creds = creds_file
+    creds = ServiceAccountCredentials.from_json_keyfile_name('../google-credentials.json', scope)
     client = gspread.authorize(creds)
     sheet = client.open("Join Interniac (Responses)").sheet1
     if not _email_exists(email, sheet):
         sheet.append_row([timezone.now().strftime('%m/%d/%Y %H:%M:%S'), email])
 
-  
 
 def _email_exists(email, sheet) -> bool:
     email_list = sheet.col_values(2)
