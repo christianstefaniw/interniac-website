37897dcefacbe517640a73387b70986bbdf70ea0
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-05-12 16:03:12 -0400

    debug false

--- careers/models.py
+++ careers/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.urls import reverse
-
+from django.utils import timezone
 
 class Career(models.Model):
     content = models.TextField()
-    posted = models.DateTimeField(null=True)
+    posted = models.DateTimeField(null=True, default=timezone.now)
 
     def get_absolute_url(self):
         return reverse('careers')
