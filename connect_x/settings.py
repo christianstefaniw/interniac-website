a9405a6f235e72786b15adf2f594a5d7e24f3107
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-12-10 18:21:27 -0500

    time fix

--- connect_x/settings.py
+++ connect_x/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG') == 'True'
 
 
 ALLOWED_HOSTS = ['www.interniac.org',
