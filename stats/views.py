8ff3fe4cf847fe41e1e49316899fb5424e374104
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-12-12 17:28:44 -0500

    fixed stats json schema

--- stats/views.py
+++ stats/views.py
@@ -6,6 +6,8 @@ from accounts.models import User
 def num_students(request):
     response_data = {
         'label': 'students',
+        'color': 'green',
+        'schemaVersion': 1,
         'message': User.objects.filter(is_student=True).count()
     }
     return JsonResponse(response_data)
@@ -14,6 +16,8 @@ def num_students(request):
 def num_employers(request):
     response_data = {
         'label': 'employers',
+        'color': 'yellowgreen',
+        'schemaVersion': 1,
         'message': User.objects.filter(is_employer=True).count()
     }
     return JsonResponse(response_data)
