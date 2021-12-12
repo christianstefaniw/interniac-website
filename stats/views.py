9d414219917ca8a2049cee2635c8f7b9abc2f0ba
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-12-12 17:26:11 -0500

    added required label in json for stats

--- stats/views.py
+++ stats/views.py
@@ -5,13 +5,15 @@ from accounts.models import User
 
 def num_students(request):
     response_data = {
+        'label': 'students',
         'message': User.objects.filter(is_student=True).count()
     }
     return JsonResponse(response_data)
 
 
-def num_employers(requst):
+def num_employers(request):
     response_data = {
+        'label': 'employers',
         'message': User.objects.filter(is_employer=True).count()
     }
     return JsonResponse(response_data)
